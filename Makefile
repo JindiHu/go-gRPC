738444942bc7c73a56157bda3b8bc3da3b6b095d
Hu Jindi <hujindi1212@gmail.com>
2022-01-11 23:16:05 +0800

    add make gen

--- Makefile
+++ Makefile
@@ -4,3 +4,6 @@ deps:
 	
 run: 
 	go run cmd/svc-auth/main.go
+
+gen:
+	protoc --proto_path=greet/greetpb/ --go_out=greet/greetpb/ --go_opt=paths=source_relative greet.proto
--- generate.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-protoc --proto_path=greet/greetpb/ --go_out=greet/greetpb/ --go_opt=paths=source_relative greet.proto
-# protoc calculator/calculatorpb/calculator.proto --go_out=plugins=grpc:.
-# protoc blog/blogpb/blog.proto --go_out=plugins=grpc:.
